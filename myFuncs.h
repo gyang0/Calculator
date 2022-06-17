f3e7a57d92b312969a1aea93483c7867a2744369
lightdrizzle <107653576+lightdrizzle@users.noreply.github.com>
2022-06-17 00:43:40 -0700

    Update myFuncs.h

--- myFuncs.h
+++ myFuncs.h
@@ -1,1 +1,12 @@
+double evalExpression(string str);
 
+
+double solveQuad(int a, int b, int c);
+// In the form ax^2 + bx + c
+// Either there are no solutions, there is one solution, or there are two solutions
+
+
+void getNumInfo(double num);
+// If the number is a positive/negative integer or a decimal
+// If the number is prime
+// Factors of the number
