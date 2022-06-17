de12f51b32d9e87051e8886dca80775eb9c6139a
lightdrizzle <107653576+lightdrizzle@users.noreply.github.com>
2022-06-17 00:42:23 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
+#include "myFuncs.h"
 using namespace std;
 
-int main(){
-  cout << "Hello world.";
-  
-  return 0;
+int main()
+{
+    int choice;
+    int num;
+    
+    cout << "Pick one:\n";
+    cout << "1. Evaluate expression\n";
+    cout << "2. Solve quadratic formula\n";
+    cout << "3. Get number info\n";
+    
+    cin >> choice;
+    
+    switch(choice)
+    {
+        case 1:
+            
+        break;
+        
+        case 2:
+            
+        break;
+        
+        case 3:
+            cout << "Enter a number: ";
+            cin >> num;
+            getNumInfo(num);
+            
+        break;
+        
+        default:
+            cout << "Try again: ";
+            cin >> choice;
+    }
+    
+    
+    return 0;
 }
