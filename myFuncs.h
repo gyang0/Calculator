2374d1fc2f0443fc7a2bcd0754ccc1d692bb9b76
lightdrizzle <107653576+lightdrizzle@users.noreply.github.com>
2022-06-17 17:15:50 -0700

    Update myFuncs.h

--- myFuncs.h
+++ myFuncs.h
@@ -1,12 +1,18 @@
-double evalExpression(string str);
+#ifndef MY_FUNCS_H
+#define MY_FUNCS_H
+
+double pythag(double a, double b);
+// Pretty obvious what this does.
 
 
-double solveQuad(int a, int b, int c);
+void solveQuad(double a, double b, double c);
 // In the form ax^2 + bx + c
-// Either there are no solutions, there is one solution, or there are two solutions
+// Either there are no solutions, there is one solution, or there are two solutions.
 
 
 void getNumInfo(double num);
-// If the number is a positive/negative integer or a decimal
+// If the number is a positive/negative integer or a decimal.
 // If the number is prime
 // Factors of the number
+
+#endif
